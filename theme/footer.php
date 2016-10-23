756ac06cb2ae43dff2f85a0bbe940c3362f8c9b4
Stefano Tamagnini <yoghi@sigmalab.net>
2016-10-23 17:14:56 +0200

    init theme

---
+++ theme/footer.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package emiroagesci
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer" role="contentinfo">
+		<div class="site-info">
+			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'emiroagesci2-0' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'emiroagesci2-0' ), 'WordPress' ); ?></a>
+			<span class="sep"> | </span>
+			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'emiroagesci2-0' ), 'emiroagesci2-0', '<a href="http://www.sigmalab.net" rel="designer">Stefano Tamagnini</a>' ); ?>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
