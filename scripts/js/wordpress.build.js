c7730065d8c52722d4b5389c7e112c5a0255784b
Stefano Tamagnini <yoghi@sigmalab.net>
2016-10-24 02:42:35 +0200

    setup complete wordpress

--- scripts/js/wordpress.build.js
+++ scripts/js/wordpress.build.js
@@ -55,6 +55,44 @@ fs.readFile('.env.dist', 'utf8', function (error,data) {
        process.stderr.write(chalk.bgRed.white(emoji.emojify("[:heavy_multiplication_x: ] Errore in scrittura .env")) + "\n");
        process.exit(1);
      }
+
+     process.stdout.write(chalk.gray(emoji.emojify('[  ] Composer update')) + "\n");
+
+     setupComposer = shell.exec('composer update',{silent:true});
+
+     if ( setupComposer.code !== 0 ){
+       process.stderr.write(chalk.gray(setupComposer.stdout+"\n"));
+       process.stderr.write(chalk.red(setupComposer.stderr+"\n"));
+       process.stderr.write(chalk.bgRed.white(emoji.emojify("[:heavy_multiplication_x: ] Errore update composer")) + "\n");
+       process.exit(1);
+     }
+
+     process.stdout.write(chalk.gray(emoji.emojify('[  ] Check wp install')) + "\n");
+
+     check = shell.exec('./wp-content/vendor/wp-cli/wp-cli/bin/wp core is-installed');
+     if ( check.code !== 0 ){
+       process.stdout.write(chalk.yellow(emoji.emojify("[:raised_hand: ] wp non installed.")) + "\n");
+       process.stdout.write(chalk.gray(emoji.emojify('[  ] wp install')) + "\n");
+
+       installWp = shell.exec('./wp-content/vendor/wp-cli/wp-cli/bin/wp core install --url='+process.env.npm_package_config_server+' --title="Develop EmiroAgesci Theme" --admin_user=admin --admin_password=admin --admin_email=webmaster@emiro.agesci.it --skip-email',{silent:true});
+
+       if ( installWp.code !== 0 ){
+         process.stderr.write(chalk.gray(installWp.stdout+"\n"));
+         process.stderr.write(chalk.red(installWp.stderr+"\n"));
+         process.stderr.write(chalk.bgRed.white(emoji.emojify("[:heavy_multiplication_x: ] Errore install wp")) + "\n");
+         process.exit(1);
+       }
+     }
+
+     enablePlugin = shell.exec('./wp-content/vendor/wp-cli/wp-cli/bin/wp plugin activate --all',{silent:true});
+     if ( enablePlugin.code !== 0 ){
+       process.stderr.write(chalk.gray(enablePlugin.stdout+"\n"));
+       process.stderr.write(chalk.red(enablePlugin.stderr+"\n"));
+       process.stdout.write(chalk.yellow(emoji.emojify("[:raised_hand: ] wp cannot load plugin.")) + "\n");
+     }
+
      process.stdout.write(chalk.bgGreen.black(emoji.emojify('[:heavy_check_mark: ] Wordpress ok!'))+ "\n");
+
   });
+
 });
